01c1b523ea50d0eb66db200a859bf90c70bb409c
masonba11 <masonba11@users.noreply.github.com>
2026-02-04 21:36:23 -0700

    Add sticky social media icons (Instagram & YouTube) in left corner
    - Created SocialMediaSticker component with fixed left position
    - Added Instagram and YouTube icons that follow the page
    - Added YouTube URL to constants
    - Styled with blue border to match call sticker

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import Header from './Header'
 import Footer from './Footer'
 import CallSticker from './CallSticker'
+import SocialMediaSticker from './SocialMediaSticker'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -9,6 +10,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <main className="flex-grow pt-24 md:pt-28 lg:pt-32">{children}</main>
       <Footer />
       <CallSticker />
+      <SocialMediaSticker />
     </div>
   )
 }
--- lib/constants.ts
+++ lib/constants.ts
@@ -25,6 +25,7 @@ export const BUSINESS_INFO = {
     facebook: 'https://www.facebook.com/profile.php?id=61586875071307',
     instagram: 'https://www.instagram.com/azwindowwashingpros/',
     tiktok: 'https://www.tiktok.com/@azwindowwashingpros?_r=1&_t=ZP-937shIX1VTL',
+    youtube: 'https://www.youtube.com/@azwindowwashingpros',
   },
 }
 
