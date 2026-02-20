1a99d6560ffa4017bc88f09f3b3c790a4910c77f
masonba11 <masonba11@users.noreply.github.com>
2026-02-19 20:56:23 -0700

    Update Google Reviews to use Google Maps link

--- components/GoogleReviewsSlider.tsx
+++ components/GoogleReviewsSlider.tsx
@@ -20,9 +20,10 @@ export default function GoogleReviewsSlider() {
               height: '600px',
             }}
           >
-            {/* Embed Google Reviews using the share link */}
+            {/* Embed Google Maps with Reviews */}
+            {/* Using the Google Maps link - may need to be converted to embed format */}
             <iframe
-              src="https://share.google.com/4tVjOOHaFaAIElHpF"
+              src="https://maps.app.goo.gl/4g8NMh5wEzVNGtDq8"
               width="100%"
               height="100%"
               style={{ border: 0 }}
@@ -38,7 +39,7 @@ export default function GoogleReviewsSlider() {
         {/* Link to view full reviews on Google */}
         <div className="text-center mt-4 pb-2">
           <a
-            href="https://share.google.com/4tVjOOHaFaAIElHpF"
+            href="https://maps.app.goo.gl/4g8NMh5wEzVNGtDq8"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-flex items-center gap-2 text-blue-600 hover:text-blue-800 text-sm font-semibold transition-colors"
@@ -49,7 +50,7 @@ export default function GoogleReviewsSlider() {
               <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"/>
               <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/>
             </svg>
-            View All Reviews on Google
+            View All Reviews on Google Maps
           </a>
         </div>
       </div>
